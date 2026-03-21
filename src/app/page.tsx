7bae0dfb4f49e27c97ba873ccdfe31f1fdce5438
kakak158 <133575206+kakak158@users.noreply.github.com>
2026-03-21 16:58:11 +0000

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,64 +2,99 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="bg-black w-screen min-h-screen text-white p-4 flex-col justify-between pb-20">
+      {/* <div className="fixed rounded-full bg-gray-950 w-14 h-14 bottom-5 right-5 border-b-3 border-gray-500 p-2 border flex justify-center text-4xl active:translate-y-0.5">
+        <h1 className="absolute top-45/100 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          +
+        </h1>
+      </div> */}
+      <div className="grid grid-cols-1 gap-4">
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
         </div>
-      </main>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+        <div className="border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-950 active:translate-y-0.5">
+          <h1 className="text-2xl font-black">My Note</h1>
+          <p className="text-gray-400 font-semibold">
+            This is a note that talks about different thi...
+          </p>
+        </div>
+      </div>
+      <div className="fixed bottom-0 left-0 h-15 w-full bg-gray-600">hi</div>
     </div>
   );
 }
