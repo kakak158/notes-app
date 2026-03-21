2c67691321ccd97881d09faaec457c0f952acaf8
kakak158 <133575206+kakak158@users.noreply.github.com>
2026-03-21 17:22:16 +0000

    Added add button to app bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-black w-screen min-h-screen text-white p-4 flex-col justify-between pb-20">
+    <div className="bg-black w-screen min-h-screen text-white p-4 flex-col justify-between pb-25">
       {/* <div className="fixed rounded-full bg-gray-950 w-14 h-14 bottom-5 right-5 border-b-3 border-gray-500 p-2 border flex justify-center text-4xl active:translate-y-0.5">
         <h1 className="absolute top-45/100 left-1/2 -translate-x-1/2 -translate-y-1/2">
           +
@@ -94,7 +94,13 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className="fixed bottom-6 left-2 right-2 h-15 border-b-3 border-gray-500 p-2 pb-2 border rounded-xl bg-gray-900"></div>
+      <div className="fixed bottom-0 left-0 right-0 h-21 bg-gray-950 border-t-2 border-gray-500 flex justify-center items-center">
+        <div className="rounded-full bg-gray-950 w-14 h-14 bottom-5 right-5 border-b-3 border-gray-500 p-2 border flex justify-center text-4xl active:translate-y-0.5">
+          <h1 className="absolute top-45/100 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            +
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
